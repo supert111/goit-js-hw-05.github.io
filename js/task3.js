7d5cfff9db2f3c28c76212de14285e784fdd997d
supert111 <31413087+supert111@users.noreply.github.com>
2020-12-28 00:53:19 +0200

    Add files via upload

---
+++ js/task3.js
@@ -0,0 +1,45 @@
+// Напиши класс Storage, который будет создавать объекты для управления
+//  складом товаров. При вызове будет получать один аргумент - начальный
+//   массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+
+class Storage {
+  constructor (items) {
+    this._items = items;
+  }
+  getItems() {
+    return this._items;
+  }
+  addItem(item) {
+    if (this._items.includes(item)) return;
+    this._items.push(item);
+  }
+  removeItem(removedItem) {
+    const newItems = [];
+    for (const item of this._items) {
+      if (item === removedItem) continue;
+      newItems.push(item);
+    }
+      this._items = newItems;
+  }
+}
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.getItems()); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.getItems()); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
---
+++ js/task5.js
@@ -0,0 +1,111 @@
+// Напиши класс Car с указанными свойствами и методами.
+class Car {
+  static getSpecs(car) {
+    console.log(`maxSpeed: ${car._maxSpeed}, speed: ${car._speed}, isOn: ${car._isOn}, distance: ${car._distance}, price: ${car._price}`);
+  }
+  /*
+   * Добавь статический метод `getSpecs(car)`,
+   * который принимает объект-машину как параметр и выводит
+   * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+   */
+
+  /*
+   * Конструктор получает объект настроек.
+   *
+   * Добавь свойства будущеего экземпляра класса:
+   *  speed - текущая скорость, изначально 0
+   *  price - цена автомобиля
+   *  maxSpeed - максимальная скорость
+   *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+   *  distance - общий киллометраж, изначально 0
+   */
+  constructor({maxSpeed, speed = 0, isOn = false, distance = 0, price}) {
+    this._speed = speed;
+    this._price = price;
+    this._maxSpeed = maxSpeed;
+    this._isOn = isOn;
+    this._distance = distance;
+  }
+
+  /*
+   * Добавь геттер и сеттер для свойства price,
+   * который будет работать с свойством цены автомобиля.
+   */
+    get price() {
+      return this._price;
+    }
+
+    set price(value) {
+
+      this._price = value;
+    }
+    
+  /*
+   * Добавь код для того чтобы завести автомобиль
+   * Записывает в свойство isOn значение true
+   */
+  turnOn() {
+    this._isOn = true;
+  }
+
+  /*
+   * Добавь код для того чтобы заглушить автомобиль
+   * Записывает в свойство isOn значение false,
+   * и сбрасывает текущую скорость в 0
+   */
+  turnOff() {
+    this._isOn = false;
+    this._speed = 0;
+  }
+
+  /*
+   * Добавялет к свойству speed полученное значение,
+   * при условии что результирующая скорость
+   * не больше чем значение свойства maxSpeed
+   */
+  accelerate(value) {
+    if (value < this._maxSpeed) {
+      this._speed = value;
+    }
+  }
+
+  /*
+   * Отнимает от свойства speed полученное значение,
+   * при условии что результирующая скорость не меньше нуля
+   */
+  decelerate(value) {
+    if (value > 0) {
+      this._speed = this._speed - value;
+    }  
+  }
+
+  /*
+   * Добавляет в поле distance киллометраж (hours * speed),
+   * но только в том случае если машина заведена!
+   */
+  drive(hours) {
+    if (this._isOn === true) {
+      this._distance += (hours * this._speed);
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
